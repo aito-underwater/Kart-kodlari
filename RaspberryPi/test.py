56f0bacd82a01cfab93afd38c70e5e6310561104
Mustafa Gülsoy <mustafa30045@gmail.com>
2022-12-06 18:12:48 +0300

    Test.0.1.16

--- RaspberryPi/test.py
+++ RaspberryPi/test.py
@@ -3,14 +3,12 @@ import time  # Module needed to add delays in the code
 
 import serial  # Module needed for serial communication
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1.0)
-
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 
 ser.flush()
 
 # Infinite loop
 import random
-
 import numpy as np
 
 a = 0
@@ -35,7 +33,6 @@ while (1):
         else:
             pass
 
-
         send_binary += str(data)
 
     # Send the string. Make sure you encode it before you send it to the Arduino.
@@ -43,7 +40,7 @@ while (1):
 
     send_binary = ''
     # Do nothing for 500 milliseconds (0.5 seconds)
-    time.sleep(0.2)
+    time.sleep(0.5)
 
     # Receive data from the Arduino
     receive_string = ser.readline().decode('utf-8').rstrip()
