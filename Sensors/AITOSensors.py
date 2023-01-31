b199b107f55a44740b303e32e9efa1865e0f905e
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-01-31 18:01:07 +0300

    Sensor kodlarının düzenlenip map dosyasıyla birleştirme 0.1.12

--- Sensors/AITOSensors.py
+++ Sensors/AITOSensors.py
@@ -330,7 +330,7 @@ def BMEData():
          main()
 
 
-if __name__ == '__main__':
+def startSensors():
   try:
         t1 = threading.Thread(target=getTFminiData1)
         t2 = threading.Thread(target=getTFminiData2)
