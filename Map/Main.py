a3354e5e7a4a93784ed9c4e687c45a472381a051
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-02-01 01:16:12 +0300

    Sensor kodlarının düzenlenip map dosyasıyla birleştirme 0.1.46

--- Map/Main.py
+++ Map/Main.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
         t2 = threading.Thread(target=Sensor.getTFminiData2)
         t22 = threading.Thread(target=Sensor.getTFminiData22)
         tWPS = threading.Thread(target=Sensor.WPSData)
-
         tMPU = threading.Thread(target=Sensor.MPUData)
         tBME = threading.Thread(target=Sensor.BMEData)
         t1.start()
@@ -23,17 +22,11 @@ if __name__ == '__main__':
         tWPS.start()
         tMPU.start()
         tBME.start()
-        print("asdsadad2")
         t1.join()
-        print("dasdsad")
         t2.join()
-        print("1")
         t22.join()
-        print("2")
         tWPS.join()
-        print("3")
         tMPU.join()
-        print("4")
         tBME.join()
 
   except KeyboardInterrupt:
@@ -46,4 +39,3 @@ if __name__ == '__main__':
     print(">>>>>>>>>>>>>>>>>>>>>>>ERROR<<<<<<<<<<<<<<<<<<<<<")
     print("-------------------------------------------------")
 
-print("2345")
