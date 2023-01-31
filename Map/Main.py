d3476d88621f805651e4a0fbb9a05d4c7cb6cd89
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-02-01 01:43:22 +0300

    Sensor kodlarının düzenlenip map dosyasıyla birleştirme 0.1.54

--- Map/Main.py
+++ Map/Main.py
@@ -1,3 +1,4 @@
+import sys
 import threading
 
 import psutil
@@ -6,15 +7,12 @@ from prettytable import PrettyTable
 import AITOSensors as Sensor
 
 
-def Test():
-    for i in range(10):
-        print(i)
+
 
 
 if __name__ == '__main__':
     try:
-        # t1 = threading.Thread(target=Sensor.getTFminiData1)
-        t1 = threading.Thread(target=Test)
+        t1 = threading.Thread(target=Sensor.getTFminiData1)
         t2 = threading.Thread(target=Sensor.getTFminiData2)
         t22 = threading.Thread(target=Sensor.getTFminiData22)
         tWPS = threading.Thread(target=Sensor.WPSData)
@@ -33,35 +31,16 @@ if __name__ == '__main__':
         tMPU.join()
         tBME.join()
 
+
     except KeyboardInterrupt:
         for proc in psutil.process_iter():
 
             if proc.name() == "pigpiod.py":
                 proc.kill()
+                sys.exit()
     except:
         print("-------------------------------------------------")
         print(">>>>>>>>>>>>>>>>>>>>>>>ERROR<<<<<<<<<<<<<<<<<<<<<")
         print("-------------------------------------------------")
 
-(chip_id, chip_version) = Sensor.BMEData().readBME280ID()
 
-temperature, pressure, humidity = Sensor.BMEData().readBME280All()
-Gx, Gy, Gz, Ax, Ay, Az = Sensor.MPUData()
-MOVE_CURSOR_UP = "\033[1A"
-ERASE = "\x1b[2K"
-
-myTable = PrettyTable(["Sensor Name:", "Value"])
-myTable.add_row(["Lidar1 cm", Sensor.getTFminiData2()])
-myTable.add_row(["Lidar2 cm", Sensor.getTFminiData1()])
-myTable.add_row(["Lidar3 cm", Sensor.getTFminiData22()])
-myTable.add_row(["Gyro Gx", Gx])
-myTable.add_row(["Gyro Gy", Gy])
-myTable.add_row(["Gyro Gz", Gz])
-myTable.add_row(["Gyro Ax", Ax])
-myTable.add_row(["Gyro Ay", Ay])
-myTable.add_row(["Gyro Az", Az])
-myTable.add_row(["Bar", Sensor.WPSData()])
-myTable.add_row(["Temperature C", temperature])
-myTable.add_row(["Pressure hPa", pressure])
-myTable.add_row(["Humidity %", humidity])
-print(myTable)
