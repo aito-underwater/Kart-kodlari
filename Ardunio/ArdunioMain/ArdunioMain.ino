f1444376aef30d17ac9937b9a52d31b445506b81
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-02-03 04:37:26 +0300

    Ardunio-Raspberry alıcı-verici kodu düzenleni ArdunioMain dosyasına eklendi 1.0.0 (Aman Dokunmayın...)

--- Ardunio/ArdunioMain/ArdunioMain.ino
+++ Ardunio/ArdunioMain/ArdunioMain.ino
@@ -1,4 +1,3 @@
-
 #include <Servo.h>
 
 // <------------ Engines params ------------> //
@@ -6,7 +5,7 @@
 #define MIN_SIGNAL 1000
 #define MOTOR_PIN1 3
 #define MOTOR_PIN2 5
-#define MOTOR_PIN3 6  
+#define MOTOR_PIN3 6
 #define MOTOR_PIN4 9
 #define MOTOR_PIN5 10
 #define MOTOR_PIN6 11
@@ -17,66 +16,35 @@ int PIDAlgoritmForEngines( Servo* engine, int power);
 
 // <------------ Communication param ------------> //
 const int BUFFER_SIZE = 48;
-char buf[BUFFER_SIZE]; 
+char buf[BUFFER_SIZE];
 
 // <------------ Engine param ------------> //
 Servo engines[6];
-int enginesPower[6] = {0,0,0,0,0,0};
+int enginesPower[6];
 
 // <------------ Loop params ------------> //
 int value, engineIndex, i, index, scale;
-    
+
 
 void setup(){
 
   Serial.begin(9600);
-  
 
-  Serial.println("Don't forget to subscribe!");
-  Serial.println("ELECTRONOOBS ESC calibration...");
-  Serial.println(" ");
-  delay(1500);
-  Serial.println("Program begin...");
-  delay(1000);
-  Serial.println("This program will start the ESC.");
+
 
   engines[0].attach(MOTOR_PIN1);
-  engines[1].attach(MOTOR_PIN1);
-  engines[2].attach(MOTOR_PIN2);
-  engines[3].attach(MOTOR_PIN3);
-  engines[4].attach(MOTOR_PIN4);
-  engines[5].attach(MOTOR_PIN5);
+  engines[1].attach(MOTOR_PIN2);
+  engines[2].attach(MOTOR_PIN3);
+  engines[3].attach(MOTOR_PIN4);
+  engines[4].attach(MOTOR_PIN5);
+  engines[5].attach(MOTOR_PIN6);
 
 
-  Serial.print("Now writing maximum output: (");Serial.print(MAX_SIGNAL);Serial.print(" us in this case)");Serial.print("\n");
-  Serial.println("Turn on power source, then wait 2 seconds and press any key.");
-
-  engines[0].writeMicroseconds(MAX_SIGNAL);
-  engines[1].writeMicroseconds(MAX_SIGNAL);
-  engines[2].writeMicroseconds(MAX_SIGNAL);
-  engines[3].writeMicroseconds(MAX_SIGNAL);
-  engines[4].writeMicroseconds(MAX_SIGNAL);
-  engines[5].writeMicroseconds(MAX_SIGNAL);
 
   // Wait for input
   while (!Serial.available());
   Serial.read();
 
-  // Send min output
-  Serial.println("\n");
-  Serial.println("\n");
-  Serial.print("Sending minimum output: (");Serial.print(MIN_SIGNAL);Serial.print(" us in this case)");Serial.print("\n");
-  engines[0].writeMicroseconds(MIN_SIGNAL);
-  engines[1].writeMicroseconds(MIN_SIGNAL);
-  engines[2].writeMicroseconds(MIN_SIGNAL);
-  engines[3].writeMicroseconds(MIN_SIGNAL);
-  engines[4].writeMicroseconds(MIN_SIGNAL);
-  engines[5].writeMicroseconds(MIN_SIGNAL);
-  Serial.println("The ESC is calibrated");
-  Serial.println("----");
-  Serial.println("Now, type a values between 1000 and 2000 and press enter");
-  Serial.println("and the motor will start rotating.");
-  Serial.println("Send 1000 to stop the motor and 2000 for full throttle");
 
 
 }
@@ -87,11 +55,11 @@ void loop(){
 
   if(Serial.available() > 0) {
     int rlen = Serial.readBytes(buf, BUFFER_SIZE);
-  
+
     value = 0;
     engineIndex = 0;
     for(index = 0; index < BUFFER_SIZE;)
-    {    
+    {
       scale = 1;
 
       for(i = 7; i >=0 ; i--)
@@ -100,7 +68,7 @@ void loop(){
         {
           scale = -1;
         }
-        else 
+        else
         {
           value = value+ pow(2,i) * (buf[index] - '0');
         }
@@ -110,7 +78,7 @@ void loop(){
       enginesPower[engineIndex] = value * 5  + 1500;
       value = 0;
       engineIndex++;
-    } 
+    }
     for(i = 0; i < 6; i++)
     {
       ChangeEngineSpeed(&engines[i],enginesPower[i]);
@@ -130,6 +98,8 @@ void loop(){
     Serial.print(" 6 : ");
   Serial.print(enginesPower[5]);
   Serial.println(" ");
+
+
     }
 
 
@@ -138,8 +108,9 @@ void loop(){
 
 void ChangeEngineSpeed( Servo* engine, int power)
 {
-  // engine->writeMicroseconds(power /* *parameters*/);
+  // engine->writeMicroseconds(power parameters);
   engine->writeMicroseconds(power);
+    Serial.println(" sadasdsad");
 }
 
 
@@ -148,7 +119,4 @@ int PIDAlgoritmForEngines( Servo* engine, int power)
 {
   return (engine->read() + (engine->read() - power) * 0.5);
 }
-
-
-
 
