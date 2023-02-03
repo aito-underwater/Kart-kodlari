c1c441b4ad15c4c83f2ed022b388f7a380c8c563
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-02-03 04:27:35 +0300

    Ardunio-Raspberry alıcı-verici kodu düzenleni ArdunioMain dosyasına eklendi 0.1.15

--- Map/BigPoolEnginPower.py
+++ Map/BigPoolEnginPower.py
@@ -1,4 +1,5 @@
-def geEnginePower(runningTime):
+import time
+def geEnginePower(runningTime, ):
     enginePower = [0] * 6
 
     enginePower[0] = 10
@@ -10,88 +11,89 @@ def geEnginePower(runningTime):
 
 
     print(runningTime)
-#
-# if runningTime < 5:  # 1
-#     enginePower[0] = 1700
-#     enginePower[1] = 1300
-#     enginePower[2] = 1300
-#     enginePower[3] = 1700
-#     enginePower[4] = 1500
-#     enginePower[5] = 1500
-#
-# elif runningTime < 10:  # 2
-#
-#     enginePower[0] = 1300
-#     enginePower[1] = 1700
-#     enginePower[2] = 1700
-#     enginePower[3] = 1300
-#     enginePower[4] = 1500
-#     enginePower[5] = 1500
-#
-# elif runningTime < 15:  # 3
-#     enginePower[0] = 1700
-#     enginePower[1] = 1700
-#     enginePower[2] = 1700
-#     enginePower[3] = 1700
-#     enginePower[4] = 1500
-#     enginePower[5] = 1500
-#
-# elif runningTime < 20:  # 4
-#     enginePower[0] = 1300
-#     enginePower[1] = 1300
-#     enginePower[2] = 1300
-#     enginePower[3] = 1300
-#     enginePower[4] = 1500
-#     enginePower[5] = 1500
-#
-# elif runningTime < 25:  # 5
-#     enginePower[0] = 1700
-#     enginePower[1] = 1500
-#     enginePower[2] = 1500
-#     enginePower[3] = 1700
-#     enginePower[4] = 1500
-#     enginePower[5] = 1500
-#
-# elif runningTime < 30:  # 6
-#     enginePower[0] = 1500
-#     enginePower[1] = 1700
-#     enginePower[2] = 1700
-#     enginePower[3] = 1500
-#     enginePower[4] = 1500
-#     enginePower[5] = 1500
-#
-# elif runningTime < 35:  # 7
-#     enginePower[0] = 1300
-#     enginePower[1] = 1500
-#     enginePower[2] = 1500
-#     enginePower[3] = 1300
-#     enginePower[4] = 1500
-#     enginePower[5] = 1500
-#
-# elif runningTime < 40:  # 8
-#     enginePower[0] = 1500
-#     enginePower[1] = 1300
-#     enginePower[2] = 1300
-#     enginePower[3] = 1500
-#     enginePower[4] = 1500
-#     enginePower[5] = 1500
-#
-# elif runningTime < 45:  # 9
-#     enginePower[0] = 1500
-#     enginePower[1] = 1500
-#     enginePower[2] = 1500
-#     enginePower[3] = 1500
-#     enginePower[4] = 2000
-#     enginePower[5] = 2000
-#
-# elif runningTime < 50:  # 10
-#     enginePower[0] = 1500
-#     enginePower[1] = 1500
-#     enginePower[2] = 1500
-#     enginePower[3] = 1500
-#     enginePower[4] = 1000
-#     enginePower[5] = 1000
-# # else:
-# #     timer = time.time()
-#     runningTime = round(time.time() - timer, 2)
-    return enginePower
+
+
+    if runningTime < 5:  # 1
+        enginePower[0] = 75
+        enginePower[1] = -25
+        enginePower[2] = -25
+        enginePower[3] = 75
+        enginePower[4] = 0
+        enginePower[5] = 0
+
+    elif runningTime < 10:  # 2
+
+        enginePower[0] = -25
+        enginePower[1] = 75
+        enginePower[2] = 75
+        enginePower[3] = -25
+        enginePower[4] = 0
+        enginePower[5] = 0
+
+    elif runningTime < 15:  # 3
+        enginePower[0] = 75
+        enginePower[1] = 75
+        enginePower[2] = 75
+        enginePower[3] = 75
+        enginePower[4] = 0
+        enginePower[5] = 0
+
+    elif runningTime < 20:  # 4
+        enginePower[0] = -25
+        enginePower[1] = -25
+        enginePower[2] = -25
+        enginePower[3] = -25
+        enginePower[4] = 0
+        enginePower[5] = 0
+
+    elif runningTime < 25:  # 5
+        enginePower[0] = 75
+        enginePower[1] = 0
+        enginePower[2] = 0
+        enginePower[3] = 75
+        enginePower[4] = 0
+        enginePower[5] = 0
+
+    elif runningTime < 30:  # 6
+        enginePower[0] = 0
+        enginePower[1] = 75
+        enginePower[2] = 75
+        enginePower[3] = 0
+        enginePower[4] = 0
+        enginePower[5] = 0
+
+    elif runningTime < 35:  # 7
+        enginePower[0] = -25
+        enginePower[1] = 0
+        enginePower[2] = 0
+        enginePower[3] = -25
+        enginePower[4] = 0
+        enginePower[5] = 0
+
+    elif runningTime < 40:  # 8
+        enginePower[0] = 0
+        enginePower[1] = -25
+        enginePower[2] = -25
+        enginePower[3] = 0
+        enginePower[4] = 0
+        enginePower[5] = 0
+
+    elif runningTime < 45:  # 9
+        enginePower[0] = 0
+        enginePower[1] = 0
+        enginePower[2] = 0
+        enginePower[3] = 0
+        enginePower[4] = 100
+        enginePower[5] = 100
+
+    elif runningTime < 50:  # 10
+        enginePower[0] = 0
+        enginePower[1] = 0
+        enginePower[2] = 0
+        enginePower[3] = 0
+        enginePower[4] = -100
+        enginePower[5] = -100
+    else:
+        timer = time.time()
+        runningTime = round(time.time() - timer, 2)
+        return enginePower
