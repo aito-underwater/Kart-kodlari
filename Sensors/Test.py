653be7ba95df86750a8652a25d305474e281cf76
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-03-17 14:22:11 +0300

    Adding Engines Tests 0.1.1

---
+++ Sensors/Test.py
@@ -0,0 +1,31 @@
+# import numpy as np
+#
+# v = np.random.rand(10)
+# v = np.array([1,1,2,1,1,1,1,-1,1,1])
+# normalized_v = v / np.sum(v)
+# print(normalized_v)
+
+
+# # Import class time from time module
+# from time import time
+#
+# start_time = time()
+# print('Start Time is :', start_time)
+# time_to_turn = 5
+# end_time = start_time + time_to_turn
+# print('End Time is :', end_time)
+# while True:
+#     print(time())
+#     if int(time()) > end_time:
+#         print('it is Okey')
+#         break;
+#
+# # Multiply 2 Python Lists using numpy
+# import numpy as np
+# numbers1 = [1, 2, 3, 4, 5]
+# numbers2 = [5, 4, 3, 2, 1]
+#
+# multiplied = list(np.multiply(numbers1, numbers2))
+# print(multiplied)
+# # Returns: [5, 8, 9, 8, 5]
+
