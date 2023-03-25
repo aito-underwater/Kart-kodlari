ad53bb3aa119f64d2982db9ebad4a4f69380e6ab
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-03-25 15:46:14 +0300

    görev testine doğru 0.1.38

--- RaspberryPi/Tasks/EnginPower.py
+++ RaspberryPi/Tasks/EnginPower.py
@@ -72,9 +72,9 @@ def normalize_data(data):
 def rotate_right(start_time):
     end_time = start_time + time_to_turn
 
-    send_data_to_engines(right_vector)
+    send_data_to_engines(turn_right_vector)
     while int(time.time()) < end_time:
-        send_data_to_engines(turn_right_vector)
+        send_data_to_engines(stop_vector)
 
 
 def rotate_random(start_time):
