3b25821158c7ed46fde0e8c3c59fe43a2cb49844
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-03-25 14:01:30 +0300

    görev testine doğru 0.1.8

--- TinkerEdgeT/raspside.py
+++ TinkerEdgeT/raspside.py
@@ -1,9 +1,18 @@
-
 import serial
+import time
+import struct
 
-ser = serial.Serial('/dev/ttyS0', 115200) # replace ttyAMA0 with the appropriate serial port
-	
+ser = serial.Serial('/dev/ttyS0', 115200, timeout=None)  # replace ttyAMA0 with the appropriate serial port
+
 while True:
-	response = ser.readline().decode('utf-8').rstrip()
-	print(response)
-	
+    # ser.flush()
+    response = ser.read(8)
+    # data_left = ser.inWaiting()  # Get the number of characters ready to be read
+    # response += ser.read(data_left)
+    # print(response)
+    # time.sleep(1)
+    data = struct.unpack('ii', response)
+    print("aaa")
+    print(data)
+
+
