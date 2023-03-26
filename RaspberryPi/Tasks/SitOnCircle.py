68d8c28305d5a4b3552df500081fa7bb7ad3fd7b
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-03-26 14:56:52 +0300

    Teknofesti kandırma keyfi 0.1.12

--- RaspberryPi/Tasks/SitOnCircle.py
+++ RaspberryPi/Tasks/SitOnCircle.py
@@ -9,13 +9,13 @@ permTimer = time.time()
 i = 10
 while True:
     timer = time.time()
-    if timer < permTimer + i * 4:
+    if timer < permTimer + i * 6:
         EnginePower.send_data_to_engines(EnginePower.turn_right_vector)
-    elif timer < permTimer + i * 5:
-        EnginePower.send_data_to_engines(EnginePower.turn_right_vector)
-    elif timer < permTimer + i * 6:
-        EnginePower.send_data_to_engines(EnginePower.forward_vector)
     elif timer < permTimer + i * 7:
-        EnginePower.send_data_to_engines(EnginePower.down_vector)
+        EnginePower.send_data_to_engines(EnginePower.turn_right_vector)
     elif timer < permTimer + i * 8:
+        EnginePower.send_data_to_engines(EnginePower.forward_vector)
+    elif timer < permTimer + i * 9:
+        EnginePower.send_data_to_engines(EnginePower.down_vector)
+    elif timer < permTimer + i * 10:
         EnginePower.send_data_to_engines(EnginePower.stop_vector)
