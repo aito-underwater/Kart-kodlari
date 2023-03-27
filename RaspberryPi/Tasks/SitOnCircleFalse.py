84079ae4cc15fdacb86814e4165c4f6d4ae815e5
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-03-27 12:20:27 +0300

    Teknofesti kandırma keyfi 0.1.48

--- RaspberryPi/Tasks/SitOnCircleFalse.py
+++ RaspberryPi/Tasks/SitOnCircleFalse.py
@@ -26,7 +26,7 @@ while True:
         EnginePower.send_data_to_engines(EnginePower.stop_vector)
     elif timer < (permTimer + (i * 4)):
         EnginePower.send_data_to_engines(np.add(EnginePower.forward_vector, EnginePower.stable_vector))
-    elif timer < (permTimer + i * 8):
+    elif timer < (permTimer + i * 7):
         EnginePower.send_data_to_engines(EnginePower.down_vector)
     elif timer < (permTimer + i * 9):
         EnginePower.send_data_to_engines(EnginePower.stop_vector)
