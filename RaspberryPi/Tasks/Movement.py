776c27962f80679a7d9bfd2864801906e80467b5
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-03-27 09:41:09 +0300

    Teknofesti kandırma keyfi 0.1.36

--- RaspberryPi/Tasks/Movement.py
+++ RaspberryPi/Tasks/Movement.py
@@ -16,14 +16,14 @@ while True:
     elif timer < permTimer + i * 2:
         EnginePower.send_data_to_engines(EnginePower.stable_vector)
 
-    elif timer < permTimer + i * 4:
+    elif timer < permTimer + i * 5:
         EnginePower.send_data_to_engines(np.add(EnginePower.stable_vector, EnginePower.forward_vector))
 
-    elif timer < permTimer + i * 6:
+    elif timer < permTimer + i * 8:
         EnginePower.send_data_to_engines(np.add(EnginePower.turn_right_vector, EnginePower.stable_vector))
 
-    elif timer < permTimer + i * 7:
+    elif timer < permTimer + i * 10:
         EnginePower.send_data_to_engines(np.add(EnginePower.stable_vector , EnginePower.forward_vector))
 
-    elif timer < permTimer + i * 10:
+    elif timer < permTimer + i * 14:
         EnginePower.send_data_to_engines(EnginePower.stop_vector)
