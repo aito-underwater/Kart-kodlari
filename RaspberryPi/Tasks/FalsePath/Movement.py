9fe4e96ad9b6a069422591dd40d1400bf800de5b
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-03-27 08:50:01 +0300

    Teknofesti kandırma keyfi 0.1.26

--- RaspberryPi/Tasks/FalsePath/Movement.py
+++ RaspberryPi/Tasks/FalsePath/Movement.py
@@ -14,28 +14,22 @@ while True:
         EnginePower.send_data_to_engines(EnginePower.stop_vector)
 
     elif timer < permTimer + i * 2:
-        EnginePower.send_data_to_engines(EnginePower.forward_vector)
+        EnginePower.send_data_to_engines(EnginePower.stable_vector)
 
     elif timer < permTimer + i * 3:
-        EnginePower.send_data_to_engines(np.negative(EnginePower.forward_vector))
+        EnginePower.send_data_to_engines(np.negative(EnginePower.stable_vector + EnginePower.forward_vector))
 
     elif timer < permTimer + i * 4:
-        EnginePower.send_data_to_engines(EnginePower.right_vector)
+        EnginePower.send_data_to_engines(EnginePower.right_vector + EnginePower.stable_vector)
 
     elif timer < permTimer + i * 5:
-        EnginePower.send_data_to_engines(np.negative(EnginePower.right_vector))
+        EnginePower.send_data_to_engines(np.negative(EnginePower.right_vector) + EnginePower.stable_vector)
 
     elif timer < permTimer + i * 6:
-        EnginePower.send_data_to_engines(EnginePower.turn_right_vector)
-
-    elif timer < permTimer + i * 7:
         EnginePower.send_data_to_engines(np.negative(EnginePower.turn_right_vector))
 
-    elif timer < permTimer + i * 8:
-        EnginePower.send_data_to_engines(EnginePower.down_vector)
+    elif timer < permTimer + i * 7:
+        EnginePower.send_data_to_engines(np.negative(EnginePower.stable_vector + EnginePower.forward_vector))
 
     elif timer < permTimer + i * 9:
         EnginePower.send_data_to_engines(EnginePower.up_vector)
-
-    else:
-        EnginePower.send_data_to_engines(EnginePower.stop_vector)
