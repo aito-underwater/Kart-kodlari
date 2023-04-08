6fdc96b9ba61a9f0d86f59579603fecffebed3f5
Mustafa <mustafa30045@gmail.com>
2023-04-08 17:40:59 +0300

    Oturma testesleri 0.1.23

--- RaspberryPi/Tasks/Algorithms/NeuralNetwork.py
+++ RaspberryPi/Tasks/Algorithms/NeuralNetwork.py
@@ -33,7 +33,7 @@ class AITONeuralNetwork:
     def load_model(self, path=None):
 
         if path is None:
-            with open('/AITO.dat', 'rb') as f:
+            with open('./AITO.dat', 'rb') as f:
                 self = pickle.load(f)
                 return self
         print(self.model_path + path)
