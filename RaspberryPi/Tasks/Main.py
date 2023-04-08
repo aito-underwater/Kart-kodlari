2bca0c84911b4cd9b74d9d3138cd8d98bf87f2db
Mustafa <mustafa30045@gmail.com>
2023-04-08 17:52:09 +0300

    Oturma testesleri 0.1.30

--- RaspberryPi/Tasks/Main.py
+++ RaspberryPi/Tasks/Main.py
@@ -36,12 +36,14 @@ def main():
 
     while True:
         ser.flush()
+
         response = ser.read(8)
         data_left = ser.inWaiting()  # Get the number of characters ready to be read
         response += ser.read(data_left)
         print(response)
         time.sleep(1)
-        data = struct.unpack('ii', response)
+        if len(response) >= 8:
+            data = struct.unpack('ii', response)
 
         if task is not None:
             print("---------------------")
