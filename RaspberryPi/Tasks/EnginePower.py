f8d7dc924e394d3770c093538deaa984c2450f3c
Mustafa <mustafa30045@gmail.com>
2023-04-09 17:08:10 +0300

    Oturma testesleri 0.1.141

--- RaspberryPi/Tasks/EnginePower.py
+++ RaspberryPi/Tasks/EnginePower.py
@@ -125,7 +125,7 @@ def set_task():
     # }
 
     # model.load_model(switcher.get(argument, "Invalid Task"))
-    model = model.load_model('Models/SitOnCircle4.dat')
+    model = model.load_model('Models/SitOnCircle5.dat')
     # return switcher.get(argument, "Invalid Task")
 
 
