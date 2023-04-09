d32c7834ae762e583545acc2cdc0821b973b39e8
Mustafa <mustafa30045@gmail.com>
2023-04-09 12:28:22 +0300

    Oturma testesleri 0.1.73

--- RaspberryPi/Tasks/EnginePower.py
+++ RaspberryPi/Tasks/EnginePower.py
@@ -126,7 +126,7 @@ def set_task():
     }
 
     # model.load_model(switcher.get(argument, "Invalid Task"))
-    model.load_model('AITO.dat')
+    model.load_model('Models/AITO.dat')
     # return switcher.get(argument, "Invalid Task")
 
 
