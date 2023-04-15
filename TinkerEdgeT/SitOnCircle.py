ae356343c86d4f447aa5badbdb1a5860f457ba94
Mustafa <mustafa30045@gmail.com>
2023-04-15 13:54:26 +0300

    Nesne Tanıma ve konumlandırma görevi 0.2.20 (Önemsiz Değiştirmeler)

--- TinkerEdgeT/SitOnCircle.py
+++ TinkerEdgeT/SitOnCircle.py
@@ -179,7 +179,7 @@ def main():
 
                 print(a, b)
                 send_data(a,b)
-        cv2.imshow("Red", frame)
+        # cv2.imshow("Red", frame)
         key = cv2.waitKey(200)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
