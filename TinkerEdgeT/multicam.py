8379f8186df1e4db0a7183c071512a4d155f3740
Mustafa <mustafa30045@gmail.com>
2023-04-15 11:51:50 +0300

    Nesne Tanıma ve konumlandırma görevi 0.2.10

--- TinkerEdgeT/multicam.py
+++ TinkerEdgeT/multicam.py
@@ -21,7 +21,7 @@ def send_data(a, b):
 
     # Do nothing for 500 milliseconds (0.5 seconds)
     # time.sleep(0.5)
-m
+
 while True:
 
     cap = cv2.VideoCapture('rtsp://admin:123456@192.168.1.237/H264?ch=1-s1?tcp&subtype=0')
