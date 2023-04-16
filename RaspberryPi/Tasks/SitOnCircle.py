9175c8521588822dd5889fe0d0495159b3ffd139
Mustafa <mustafa30045@gmail.com>
2023-04-16 10:50:08 +0300

    Nesne Tanıma ve konumlandırma görevi 0.2.50

--- RaspberryPi/Tasks/SitOnCircle.py
+++ RaspberryPi/Tasks/SitOnCircle.py
@@ -87,7 +87,7 @@ def main():
                 next_move = EnginePower.select_vector(power_vector)
                 if next_move is True:
                     go_down = True
-                    timer = time.time() + 3
+                    timer = time.time() + 1
 
                 else:
                     EnginePower.send_data_to_engines(next_move)
