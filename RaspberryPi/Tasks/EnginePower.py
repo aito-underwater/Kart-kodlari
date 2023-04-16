f1d74aed831d9f67b4ed14f22a7aa07794825129
Mustafa <mustafa30045@gmail.com>
2023-04-16 11:13:15 +0300

    Nesne Tanıma ve konumlandırma görevi 0.2.56

--- RaspberryPi/Tasks/EnginePower.py
+++ RaspberryPi/Tasks/EnginePower.py
@@ -125,7 +125,7 @@ def set_task():
     # }
 
     # model.load_model(switcher.get(argument, "Invalid Task"))
-    model = model.load_model('Models/SitOnCircle26.dat')
+    model = model.load_model('Models/SitOnCircle27.dat')
     # return switcher.get(argument, "Invalid Task")
 
 
