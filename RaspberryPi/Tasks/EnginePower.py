7667a983475b7921bd285d955de2f097ea379260
Oğuzhan Turhan <oguzhant465@gmail.com>
2023-04-21 23:41:25 +0300

    oguz_test 57

--- RaspberryPi/Tasks/EnginePower.py
+++ RaspberryPi/Tasks/EnginePower.py
@@ -18,9 +18,9 @@ model = nn.AITONeuralNetwork(input_layer_size, secret_layer_size, secret_layer_c
 #  [-sağ arka , sol arka, sağ orta, -sol orta, -sağ ön, -sol ön]
 
 # Main movement vectors
-forward_vector = [-50, 50, 0, 0, -50, -50]
-right_vector = [-50, -50, 0, 0, 50, -50]
-turn_right_vector = [50, 50, 0, 0, 50, -50]
+forward_vector = [-25, 25, 0, 0, -25, -25]
+right_vector = [-25, -25, 0, 0, 25, -25]
+turn_right_vector = [25, 25, 0, 0, 25, -25]
 down_vector = [0, 0, 20, 20, 0, 0]
 up_vector = [0, 0, -30, -30, 0, 0]
 stable_vector = [0, 0, 14, -14, 0, 0]
