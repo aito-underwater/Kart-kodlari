51901f97e06953e5484bfada0878a60c488901da
Mustafa <mustafa30045@gmail.com>
2023-04-21 16:27:20 +0300

    Nesne Tanıma ve konumlandırma görevi 0.2.103

--- TrainCodes/AI_Train_Script.py
+++ TrainCodes/AI_Train_Script.py
@@ -6,14 +6,17 @@ from RaspberryPi.Tasks.Algorithms.NeuralNetwork import AITONeuralNetwork as nn
 
 input_layer_size = 2
 secret_layer_size = 4
-secret_layer_count = 2
+secret_layer_count = 16
 generation_count = 20
 
-df = pd.read_csv('Datas/main_test.csv')
 
-x = np.array(pd.DataFrame(df, columns=["X", "Y"]))
+df = pd.read_csv('Datas/mustafa_test2.csv')
 
-y = np.array(pd.DataFrame(df, columns=["forward", "right", "rotate", "down"]))
+
+print()
+x = np.array(df.iloc[:,0:2])
+
+y = np.array(df.iloc[:,2:6])
 
 # df = np.array(pd.DataFrame(df, columns = ["X","Y","forward","right","rotate","down"]))
 
@@ -32,56 +35,84 @@ test_nn = nn(input_layer_size, secret_layer_size, secret_layer_count,
              generation_count)
 test_nn.set_up()
 
-test_nn.fit(input_x=x,
-            output_y=y, iteration=1000, genetic_iteration=10)
-
-test_nn.save_model('Models/SitOnCircle_emirhan_1.dat')
-
 
 test_nn.fit(input_x=x,
-            output_y=y, iteration=1000, genetic_iteration=10)
-test_nn.save_model('Models/SitOnCircle_emirhan_2.dat')
+            output_y=y, iteration=1, genetic_iteration=1)
 
-
-test_nn.fit(input_x=x,
-            output_y=y, iteration=1000, genetic_iteration=10)
-test_nn.save_model('Models/SitOnCircle_emirhan_3.dat')
-
-
-test_nn.fit(input_x=x,
-            output_y=y, iteration=1000, genetic_iteration=10)
-test_nn.save_model('Models/SitOnCircle_emirhan_4.dat')
-
-
-test_nn.fit(input_x=x,
-            output_y=y, iteration=1000, genetic_iteration=10)
-test_nn.save_model('Models/SitOnCircle_emirhan_5.dat')
-
-
-test_nn.fit(input_x=x,
-            output_y=y, iteration=1000, genetic_iteration=10)
-test_nn.save_model('Models/SitOnCircle_emirhan_6.dat')
-
-
-test_nn.fit(input_x=x,
-            output_y=y, iteration=1000, genetic_iteration=10)
-test_nn.save_model('Models/SitOnCircle_emirhan_7.dat')
+test_nn.save_model('Models/SitOnCircle_mustafa_16_1.dat')
 
 
 
 test_nn.fit(input_x=x,
-            output_y=y, iteration=1000, genetic_iteration=10)
-test_nn.save_model('Models/SitOnCircle_emirhan_8.dat')
+            output_y=y, iteration=10, genetic_iteration=1)
+
+test_nn.save_model('Models/SitOnCircle_mustafa_16_2.dat')
+
+
+
+test_nn.fit(input_x=x,
+            output_y=y, iteration=100, genetic_iteration=1)
+
+test_nn.save_model('Models/SitOnCircle_mustafa_16_2.dat')
+
+
+
+test_nn.fit(input_x=x,
+            output_y=y, iteration=1000, genetic_iteration=1)
+
+test_nn.save_model('Models/SitOnCircle_mustafa_16_3.dat')
+
+
+
+test_nn.fit(input_x=x,
+            output_y=y, iteration=10000, genetic_iteration=1)
+
+test_nn.save_model('Models/SitOnCircle_mustafa_16_4.dat')
+
+
+
+test_nn.fit(input_x=x,
+            output_y=y, iteration=1, genetic_iteration=10)
+
+test_nn.save_model('Models/SitOnCircle_mustafa_16_5.dat')
+
+
+
+test_nn.fit(input_x=x,
+            output_y=y, iteration=10, genetic_iteration=10)
+
+test_nn.save_model('Models/SitOnCircle_mustafa_16_6.dat')
+
+
+
+test_nn.fit(input_x=x,
+            output_y=y, iteration=100, genetic_iteration=10)
+
+test_nn.save_model('Models/SitOnCircle_mustafa_16_7.dat')
+
 
 
 test_nn.fit(input_x=x,
             output_y=y, iteration=1000, genetic_iteration=10)
-test_nn.save_model('Models/SitOnCircle_emirhan_9.dat')
+
+test_nn.save_model('Models/SitOnCircle_mustafa_16_8.dat')
+
 
 
 test_nn.fit(input_x=x,
-            output_y=y, iteration=1000, genetic_iteration=10)
-test_nn.save_model('Models/SitOnCircle_emirhan_10.dat')
+            output_y=y, iteration=10, genetic_iteration=100)
+
+test_nn.save_model('Models/SitOnCircle_mustafa_16_9.dat')
+
+
+
+test_nn.fit(input_x=x,
+            output_y=y, iteration=100, genetic_iteration=100)
+
+test_nn.save_model('Models/SitOnCircle_mustafa_16_10.dat')
+
+
+
 
 # Hangi dosyada çaışıyorsak o dosyanın tolu üzerinden işlem yapmamız gerekiyor
 # test_nn = test_nn.load_model("AITO1.dat")
