af50cbb527e0d3b17716cfb14feca7335bb9b951
Mustafa <mustafa30045@gmail.com>
2023-04-22 15:51:15 +0300

    Nesne Tanıma ve konumlandırma görevi 0.2.118

--- Ardunio/ArdunioMain/ArdunioMain.ino
+++ Ardunio/ArdunioMain/ArdunioMain.ino
@@ -179,7 +179,7 @@ void ChangeEngineSpeed( ServoEngine* engine, int power)
 int PIDAlgorithmForEngines( ServoEngine* engine, int power)
 {
 
-  int newPower =  engine->power + 1000/(targetTime - running_time ) * (engine->power - power) * 0.5;
+     int newPower =  engine->power + 1000/(targetTime - running_time ) * (engine->power - power) * 0.5;
   return (power);
 }
 
