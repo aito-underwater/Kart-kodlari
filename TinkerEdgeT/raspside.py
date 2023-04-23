b9083644c15f400b02a2c383523c80049b963c15
Oğuzhan Turhan <oguzhant465@gmail.com>
2023-04-23 13:26:12 +0300

    Oguz_test 175

--- TinkerEdgeT/raspside.py
+++ TinkerEdgeT/raspside.py
@@ -5,7 +5,7 @@ import struct
 ser = serial.Serial('/dev/ttyS0', 115200, timeout=None)  # replace ttyAMA0 with the appropriate serial port
 
 while True:
-    # ser.flush()
+    ser.flush()
     response = ser.read(8)
     # data_left = ser.inWaiting()  # Get the number of characters ready to be read
     # response += ser.read(data_left)
