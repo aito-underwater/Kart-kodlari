f4b8da3553b9ada2eb8761fe908afefb249aecd5
Mustafa <mustafa30045@gmail.com>
2023-04-28 10:43:42 +0300

    Nesne Tanıma ve konumlandırma görevi 0.2.232

--- RaspberryPi/Tasks/SitOnCircle.py
+++ RaspberryPi/Tasks/SitOnCircle.py
@@ -34,7 +34,7 @@ def main():
     go_down = False
     ser = serial.Serial('/dev/ttyS0', 115200, timeout=0)  # replace ttyAMA0 with the appropriate serial port
 
-    EnginePower.set_task('Models/sitCircle/AITO.dat')
+    EnginePower.set_task('Models/test/AITO.dat')
     timer = time.time()
     searchTimer = time.time() +20
     data = []
