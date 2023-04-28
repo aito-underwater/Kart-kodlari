52bbd02e8d294bb3cbf52513c71ae813e21fa52a
Oğuzhan Turhan <oguzhant465@gmail.com>
2023-04-28 14:10:46 +0300

    ozzy_Teknofest Test v9-28 ezgi 6

--- RaspberryPi/Tasks/SitOnCircle.py
+++ RaspberryPi/Tasks/SitOnCircle.py
@@ -34,7 +34,7 @@ def main():
     go_down = False
     ser = serial.Serial('/dev/ttyS0', 115200, timeout=0)  # replace ttyAMA0 with the appropriate serial port
 
-    EnginePower.set_task('Models/SitOnCircle_mustafa_16_4.dat')
+    EnginePower.set_task('Models/SitOnCircle_ezgi_8_6.dat')
     timer = time.time()
     searchTimer = time.time() +20
     data = []
